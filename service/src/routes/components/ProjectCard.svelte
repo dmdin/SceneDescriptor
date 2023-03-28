93af0f08caeeae3015acee5a97061d51ea134a96
Dmitriy Din <dmitriy1d01@gmail.com>
2023-03-28 13:29:52 +0300

    Finished project preview

--- service/src/routes/components/ProjectCard.svelte
+++ service/src/routes/components/ProjectCard.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
+  import {onMount} from "svelte";
+
   export let description
   export let video
   export let preview
   export let audio
-  
+
+  let name = ''
+
+  onMount(async () => {
+    name = await fetch(description).then(r => r.json())
+  })
+
 </script>
 
-<p>{preview}</p>
-<div class="bg-gray-800">
-  <img src={preview} alt="Превью">
-  <h2>Матрица</h2>
+<div class="max-w-sm bg-gray-900 rounded-lg shadow border border-2 border-transparent transition transition-colors hover:border-orange-600 cursor-pointer">
+  <img class="rounded-t-lg" src={preview} alt="Preview"/>
+  <div class="p-2">
+    <h5 class="mb-1 text-xl font-normal text-center tracking-tight text-gray-300">Матрица</h5>
+  </div>
 </div>
+
+<style>
+</style>
