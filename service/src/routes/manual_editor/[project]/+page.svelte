4d1014996e3e9f96731594d02da050453f95d297
Dmitriy Din <dmitriy1d01@gmail.com>
2023-03-28 14:56:14 +0300

    Updated video editor

--- service/src/routes/manual_editor/[project]/+page.svelte
+++ service/src/routes/manual_editor/[project]/+page.svelte
@@ -3,7 +3,7 @@
   import {writable} from "svelte/store";
   import {page} from '$app/stores';
   import {CUTTER_URL} from "$lib/constants";
-
+  import PointEditor from "./PointEditor.svelte";
   import {Play, Pause, Microphone, Bookmark} from 'svelte-heros-v2'
 
   const wavesurfer: any = writable()
@@ -186,19 +186,24 @@
         <source src={videoUrl}>
         <track kind="captions">
       </video>
-      <div>
-        <button class="text-gray-300 transition transition-color hover:text-orange-600"
-                on:click={() => {paused=true}}>
-          <Pause variation="solid"/>
-        </button>
-        <button class="text-gray-300 transition transition-color hover:text-orange-600"
-                on:click={() => {paused=false}}>
-          <Play variation="solid"/>
-        </button>
-        <button class="text-gray-300 transition transition-color hover:text-orange-600"
-                on:click={() => () => new_point(time_now)}>
-          <Bookmark variation="solid"/>
-        </button>
+      <div class="flex justify-between w-full px-4">
+        <div class="flex gap-2">
+          <button class="text-gray-300 transition transition-color hover:text-orange-600"
+                  class:text-orange-500={paused}
+                  on:click={() => {paused=true}}>
+            <Pause variation="solid"/>
+          </button>
+          <button class="text-gray-300 transition transition-color hover:text-orange-600"
+                  class:text-orange-500={!paused}
+                  on:click={() => {paused=false}}>
+            <Play variation="solid"/>
+          </button>
+          <button class="text-gray-300 transition transition-color hover:text-orange-600"
+                  on:click={() => new_point(time_now)}>
+            <Bookmark variation="solid" size="20"/>
+          </button>
+        </div>
+        <p class="text-center my-1 text-gray-400">{format(time_now)} / {format(duration)}</p>
       </div>
     </div>
     <div class="p-2 flex flex-col shadow-xl rounded-lg">
@@ -210,71 +215,68 @@
 
       <button class="btn-blue mt-1" on:click={start_video}>Запустить видео с заметками</button>
 
+      <PointEditor/>
+      <!--{#if point_voc[time_now - (time_now % 1)]}-->
+      <!--  <div class="border mt-2 p-1 rounded-lg flex flex-col">-->
+      <!--    <p class="text-xs text-gray-400 text-center">Редактирование маркера</p>-->
+      <!--    <div class="flex">-->
+      <!--      <p class="mr-1 text-gray-600 text-sm my-auto">Текст отметки:</p>-->
+      <!--      <input class="p-0.5 rounded-lg" type="text" bind:value={point_voc[time_now - (time_now % 1)].name}>-->
+      <!--    </div>-->
+      <!--    <p on:click={() => delete_point(time_now)} class="text-xs text-red-500 hover:text-red-400 cursor-pointer">-->
+      <!--      Удалить отметку</p>-->
 
-      {#if point_voc[time_now - (time_now % 1)]}
-        <div class="border mt-2 p-1 rounded-lg flex flex-col">
-          <p class="text-xs text-gray-400 text-center">Настройка отметки(?)</p>
-          <div class="flex">
-            <p class="mr-1 text-gray-600 text-sm my-auto">Название - </p>
-            <input class="p-0.5 rounded-lg" type="text" bind:value={point_voc[time_now - (time_now % 1)].name}>
-          </div>
-          <p on:click={() => delete_point(time_now)} class="text-xs text-red-500 hover:text-red-400 cursor-pointer">
-            Удалить отметку</p>
 
+      <!--    {#if point_voc[time_now - (time_now % 1)].audio_blob !== null}-->
 
-          {#if point_voc[time_now - (time_now % 1)].audio_blob !== null}
-
-            <div class="flex h-20">
-              <audio class="my-auto" src={URL.createObjectURL(point_voc[time_now - (time_now % 1)].audio_blob)}
-                     controls></audio>
-              <button class="bg-red-400 hover:bg-red-300 w-8 h-8 my-auto ml-2 rounded-xl"
-                      on:click={() => {point_voc[time_now - (time_now % 1)].audio_blob = null}}>
-                <svg width="16" height="16" class="m-auto" viewBox="0 0 16 16" fill="none"
-                     xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="M0.472396 0.472518C0.758152 0.186848 1.14567 0.0263672 1.54973 0.0263672C1.95379 0.0263672 2.34131 0.186848 2.62706 0.472518L8.01525 5.86071L13.4034 0.472518C13.6908 0.194944 14.0758 0.0413523 14.4753 0.0448241C14.8748 0.048296 15.257 0.208553 15.5395 0.49108C15.8221 0.773607 15.9823 1.1558 15.9858 1.55534C15.9893 1.95487 15.8357 2.33979 15.5581 2.62718L10.1699 8.01537L15.5581 13.4036C15.8357 13.691 15.9893 14.0759 15.9858 14.4754C15.9823 14.875 15.8221 15.2571 15.5395 15.5397C15.257 15.8222 14.8748 15.9825 14.4753 15.9859C14.0758 15.9894 13.6908 15.8358 13.4034 15.5582L8.01525 10.17L2.62706 15.5582C2.33967 15.8358 1.95475 15.9894 1.55521 15.9859C1.15568 15.9825 0.773485 15.8222 0.490958 15.5397C0.208431 15.2571 0.048174 14.875 0.0447021 14.4754C0.0412302 14.0759 0.194822 13.691 0.472396 13.4036L5.86059 8.01537L0.472396 2.62718C0.186726 2.34143 0.0262451 1.95391 0.0262451 1.54985C0.0262451 1.14579 0.186726 0.758274 0.472396 0.472518Z"
-                    fill="#ffffff"/>
-                </svg>
-              </button>
-            </div>
-          {:else}
-            {#if !recording_state}
-              <button class="btn-blue mt-2 mx-auto" on:click={() => start_recording(time_now)}>
-                <Microphone/>
-              </button>
-            {:else}
-              <button class="btn-blue mt-2 mx-auto" on:click={stop_recording}>Остановить запись</button>
-            {/if}
-          {/if}
-
-        </div>
-      {:else}
-        <button on:click={() => new_point(time_now)} class="btn-blue mt-2 mx-auto">Создать отметку(?)</button>
-      {/if}
+      <!--      <div class="flex h-20">-->
+      <!--        <audio class="my-auto" src={URL.createObjectURL(point_voc[time_now - (time_now % 1)].audio_blob)}-->
+      <!--               controls></audio>-->
+      <!--        <button class="bg-red-400 hover:bg-red-300 w-8 h-8 my-auto ml-2 rounded-xl"-->
+      <!--                on:click={() => {point_voc[time_now - (time_now % 1)].audio_blob = null}}>-->
+      <!--          <svg width="16" height="16" class="m-auto" viewBox="0 0 16 16" fill="none"-->
+      <!--               xmlns="http://www.w3.org/2000/svg">-->
+      <!--            <path-->
+      <!--              d="M0.472396 0.472518C0.758152 0.186848 1.14567 0.0263672 1.54973 0.0263672C1.95379 0.0263672 2.34131 0.186848 2.62706 0.472518L8.01525 5.86071L13.4034 0.472518C13.6908 0.194944 14.0758 0.0413523 14.4753 0.0448241C14.8748 0.048296 15.257 0.208553 15.5395 0.49108C15.8221 0.773607 15.9823 1.1558 15.9858 1.55534C15.9893 1.95487 15.8357 2.33979 15.5581 2.62718L10.1699 8.01537L15.5581 13.4036C15.8357 13.691 15.9893 14.0759 15.9858 14.4754C15.9823 14.875 15.8221 15.2571 15.5395 15.5397C15.257 15.8222 14.8748 15.9825 14.4753 15.9859C14.0758 15.9894 13.6908 15.8358 13.4034 15.5582L8.01525 10.17L2.62706 15.5582C2.33967 15.8358 1.95475 15.9894 1.55521 15.9859C1.15568 15.9825 0.773485 15.8222 0.490958 15.5397C0.208431 15.2571 0.048174 14.875 0.0447021 14.4754C0.0412302 14.0759 0.194822 13.691 0.472396 13.4036L5.86059 8.01537L0.472396 2.62718C0.186726 2.34143 0.0262451 1.95391 0.0262451 1.54985C0.0262451 1.14579 0.186726 0.758274 0.472396 0.472518Z"-->
+      <!--              fill="#ffffff"/>-->
+      <!--          </svg>-->
+      <!--        </button>-->
+      <!--      </div>-->
+      <!--    {:else}-->
+      <!--      {#if !recording_state}-->
+      <!--        <button class="btn-blue mt-2 mx-auto" on:click={() => start_recording(time_now)}>-->
+      <!--          <Microphone/>-->
+      <!--        </button>-->
+      <!--      {:else}-->
+      <!--        <button class="btn-blue mt-2 mx-auto" on:click={stop_recording}>Остановить запись</button>-->
+      <!--      {/if}-->
+      <!--    {/if}-->
+      <!--  </div>-->
+      <!--{:else}-->
+      <!--  <button on:click={() => new_point(time_now)} class="btn-blue mt-2 mx-auto">Создать отметку(?)</button>-->
+      <!--{/if}-->
     </div>
   </div>
 
 
   <div class="flex flex-col shadow-xl mt-2 pb-2 rounded-lg">
-
-
-    <p class="text-center my-1">{format(time_now)} / {format(duration)}</p>
-
     <div class="flex px-4 text-lg">
-      <div class="w-full mx-4 my-auto bg-gray-200 rounded-full h-2.5 dark:bg-gray-700"
+      <div class="w-full mx-4 my-auto bg-gray-200 rounded-full h-2.5 dark:bg-gray-700 cursor-pointer"
            on:mousemove={handleMove}
            on:touchmove|preventDefault={handleMove}
            on:mousedown={handleMousedown}
            on:mouseup={handleMouseup}>
-        <div class="bg-blue-600 h-2.5 rounded-full" style={"width: " + ((time_now / duration) * 100) + "%;"}></div>
+        <div class="bg-orange-600 h-2.5" style={"width: " + ((time_now / duration) * 100) + "%;"}></div>
       </div>
     </div>
 
     <div class="flex px-4 mt-1 mb-4 text-lg bg-blue-100">
       <div class="flex mx-4 w-full relative bg-green-100 ">
         {#each point_list as point}
-          <div class="w-2 h-2 bg-red-600 absolute cursor-pointer rounded-full" on:click={() => time_now = point}
-               style={"left: calc(" + ((point / duration) * 100) + "% - 8px);"}></div>
+          <button class="absolute cursor-pointer" on:click={() => time_now = point}
+               style={"left: calc(" + ((point / duration) * 100) + "% - 8px);"}>
+            <Bookmark size="15" class="text-orange-800" variation="solid"/>
+          </button>
         {/each}
       </div>
     </div>
---
+++ service/src/routes/manual_editor/[project]/PointEditor.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import {Trash, Microphone, Stop} from 'svelte-heros-v2'
+
+  export let description
+</script>
+
+
+<div class="border mt-2 p-1 rounded-lg flex flex-col">
+  <p class="text-xs text-gray-400 text-center">Редактирование маркера</p>
+  <div class="flex">
+    <p class="mr-1 text-gray-600 text-sm my-auto">Текст отметки:</p>
+    <textarea class="p-0.5 rounded-lg"></textarea>
+    <input class="p-0.5 rounded-lg" type="text" bind:value={description}>
+  </div>
+  <button class="text-xs text-red-500 hover:text-red-400 cursor-pointer">
+    <Trash/>
+  </button>
+
+
+  <div class="flex h-20">
+    <!--              <audio class="my-auto" src={URL.createObjectURL(point_voc[time_now - (time_now % 1)].audio_blob)}-->
+    <!--                     controls></audio>-->
+    <button class="bg-red-400 hover:bg-red-300 w-8 h-8 my-auto ml-2 rounded-xl"
+            on:click={() => console.log(1)}>
+      <svg width="16" height="16" class="m-auto" viewBox="0 0 16 16" fill="none"
+           xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M0.472396 0.472518C0.758152 0.186848 1.14567 0.0263672 1.54973 0.0263672C1.95379 0.0263672 2.34131 0.186848 2.62706 0.472518L8.01525 5.86071L13.4034 0.472518C13.6908 0.194944 14.0758 0.0413523 14.4753 0.0448241C14.8748 0.048296 15.257 0.208553 15.5395 0.49108C15.8221 0.773607 15.9823 1.1558 15.9858 1.55534C15.9893 1.95487 15.8357 2.33979 15.5581 2.62718L10.1699 8.01537L15.5581 13.4036C15.8357 13.691 15.9893 14.0759 15.9858 14.4754C15.9823 14.875 15.8221 15.2571 15.5395 15.5397C15.257 15.8222 14.8748 15.9825 14.4753 15.9859C14.0758 15.9894 13.6908 15.8358 13.4034 15.5582L8.01525 10.17L2.62706 15.5582C2.33967 15.8358 1.95475 15.9894 1.55521 15.9859C1.15568 15.9825 0.773485 15.8222 0.490958 15.5397C0.208431 15.2571 0.048174 14.875 0.0447021 14.4754C0.0412302 14.0759 0.194822 13.691 0.472396 13.4036L5.86059 8.01537L0.472396 2.62718C0.186726 2.34143 0.0262451 1.95391 0.0262451 1.54985C0.0262451 1.14579 0.186726 0.758274 0.472396 0.472518Z"
+          fill="#ffffff"/>
+      </svg>
+    </button>
+  </div>
+
+  <button class="btn-blue mt-2 mx-auto">
+    <Microphone/>
+  </button>
+  <button class="btn-blue mt-2 mx-auto" on:click={() => console.log(2)}>
+    <Stop/>
+  </button>
+</div>
