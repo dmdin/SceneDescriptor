32f2a6338170a50aadc954bec23e477d98ead081
kuvshinovmark <kuvshinovmark2001@gmail.com>
2023-03-29 13:26:00 +0300

    uploader

--- service/src/lib/components/ProjectCard.svelte
+++ service/src/lib/components/ProjectCard.svelte
@@ -12,11 +12,10 @@
 
 </script>
 
-<div class="{$$props.class} bg-gray-900 rounded-lg shadow border border-2 border-transparent transition transition-colors hover:border-orange-600 cursor-pointer"
-  on:click={() => goto(`/manual_editor/${id}`)}
->
-  <img class="rounded-t-lg" src={preview} alt="Preview"/>
-  <div class="p-2">
+<!-- <div class="{$$props.class} bg-gray-900 rounded-lg shadow border border-2 border-transparent transition transition-colors hover:border-orange-600 cursor-pointer" on:click={() => goto(`/manual_editor/${id}`)}> -->
+<div class="{$$props.class} bg-gray-900 flex w-full flex-col rounded-lg shadow border-2 border-transparent transition-colors hover:border-orange-600 cursor-pointer" on:click={() => goto(`/manual_editor/${id}`)}>
+  <img class="rounded-t-lg my-auto" src={preview} alt="Preview"/>
+  <div class="p-2 mt-auto">
     <h5 class="mb-1 {textCls} font-normal text-center tracking-tight text-gray-500">{name}</h5>
   </div>
 </div>
--- service/src/routes/+page.svelte
+++ service/src/routes/+page.svelte
@@ -1,29 +1,40 @@
 <script lang="ts">
   import ProjectCard from '$lib/components/ProjectCard.svelte'
+  import Uploader from '$lib/components/Uploader.svelte';
   import {CUTTER_URL} from "$lib/constants";
   import {onMount} from "svelte";
 
   let projects: unknown[] = []
-
+  
   onMount(async () => {
-    projects = await fetch(new URL('/get_all_projects', CUTTER_URL)).then(r => r.json())
+    getProjects();
   })
+  
+  async function getProjects(){
+    projects = await fetch(new URL('/get_all_projects', CUTTER_URL)).then(r => r.json())
+    console.log(projects);
+  }
+
 </script>
 
 <svelte:head>
 	<title>Редактор видео</title>
 	<meta name="description" content="Video Editor" />
 </svelte:head>
+
 <div class="flex flex-col items-center w-full h-screen">
   <h1 class="text-4xl font-black text-gray-500 text-center mt-20">Выбор проекта</h1>
 
   <div class="bg-gray-900 w-4/5 grid grid-cols-2 gap-5 place-items-center mt-10 max-w-5xl">
-<!--    <p>{JSON.stringify(projects)}</p>-->
     {#each projects as project}
       <ProjectCard {...project}/>
     {/each}
   </div>
+
+  <Uploader on:success_creation={getProjects}/>
+  
 </div>
+
 <style>
 
 </style>
